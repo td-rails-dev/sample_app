8d52bfcfe5006c85998a50967ca41071f248d69a
td-rails-dev <td.rails.dev@gmail.com>
2014-06-19 11:57:04 +0900

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,49 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  describe "Home page" do
-    it "should have the content 'Sample App" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+
+  describe "Home Page" do
+
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_title('Ruby on Rails Tutorial Sample App')
+    end
+
+    it "should not have a custom page title" do
+     visit '/static_pages/home'
+     page.should_not have_title('| Home')
     end
   end
+
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+      it "should have the title 'Help'" do 
+        visit '/static_pages/help' 
+        page.should have_title("Ruby on Rails Tutorial Sample App | Help")
+    end 
+
+  end
+
+
+  describe "About Page" do
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_title("Ruby on Rails Tutorial Sample App | About")
+    end
+  end
+
 end
